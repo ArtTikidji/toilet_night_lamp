59bed7fc5ad55465c6adc5b5d0b726bf89a78b4f
ArtTikidji <67237375+ArtTikidji@users.noreply.github.com>
2022-08-08 20:45:52 +0400

    Update toilet_night_lamp.ino
    small improves + turning off light just once after activation

--- toilet_night_lamp.ino
+++ toilet_night_lamp.ino
@@ -4,32 +4,45 @@
 #define PIN 8   // light control pin
 #define PIN_NUM 2 //count of leds
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel( PIN_NUM,PIN, NEO_GRB + NEO_KHZ800);
-
 int state;
-int r, g;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIN_NUM, PIN, NEO_GRB + NEO_KHZ800);
+
+
+void smoothly_turning_on_light(){
+  for(int brightness = 0; brightness < 255; brightness++){
+      strip.setPixelColor(0, strip.Color(brightness, brightness, 0));
+      strip.show();
+      delay(100);
+    }
+}
+
+void turn_off_light(){
+  strip.setPixelColor(0, strip.Color(0, 0, 0));
+  strip.show();
+}
+
 void setup()
-{
+{   
   pinMode(SENSOR_PIN, INPUT);
   Serial.begin(9600);
   strip.begin();
+  
+  // in start position turning off light
+  turn_off_light();
 }
+
+
 void loop()
 {
   state = digitalRead(SENSOR_PIN);
   if (state == 1){
     Serial.println("Somebody is in this area!");
-    for(r = 0; r < 255; r++){
-      g = r;
-      strip.setPixelColor(0, strip.Color(r, g, 0));//setting yellow colour
-      strip.show();
-      delay(100);
-    }
+
+    smoothly_turning_on_light();
+
     delay(120000);
-  }
-  else{
-    strip.setPixelColor(0, strip.Color(0, 0, 0));//turn off light
-    strip.show();
-    delay(500);
+    
+    // after some time turning off light
+    turn_off_light();
   }
 }
