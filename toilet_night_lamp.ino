04ae82bbd5b77e0e63b9c3c160faeff71ac1f4cf
ArtTikidji <67237375+ArtTikidji@users.noreply.github.com>
2022-08-08 20:20:19 +0400

    Update toilet_night_lamp.ino
    translated comments + small improves

--- toilet_night_lamp.ino
+++ toilet_night_lamp.ino
@@ -1,8 +1,8 @@
 #include <Adafruit_NeoPixel.h>
 
-#define  sensorPin  6
-#define PIN 8   //led灯控制引脚
-#define PIN_NUM 2 //允许接的led灯的个数
+#define SENSOR_PIN  6
+#define PIN 8   // light control pin
+#define PIN_NUM 2 //count of leds
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel( PIN_NUM,PIN, NEO_GRB + NEO_KHZ800);
 
@@ -10,26 +10,26 @@ int state;
 int r, g;
 void setup()
 {
-  pinMode(sensorPin, INPUT);
+  pinMode(SENSOR_PIN, INPUT);
   Serial.begin(9600);
   strip.begin();
 }
 void loop()
 {
-  state = digitalRead(sensorPin);
+  state = digitalRead(SENSOR_PIN);
   if (state == 1){
     Serial.println("Somebody is in this area!");
     for(r = 0; r < 255; r++){
       g = r;
-      strip.setPixelColor(0, strip.Color(r, g, 0));//黄光
+      strip.setPixelColor(0, strip.Color(r, g, 0));//setting yellow colour
       strip.show();
       delay(100);
     }
     delay(120000);
   }
   else{
-  strip.setPixelColor(0, strip.Color(0, 0, 0));//灭
-  strip.show();
-  delay(500);
+    strip.setPixelColor(0, strip.Color(0, 0, 0));//turn off light
+    strip.show();
+    delay(500);
   }
 }
