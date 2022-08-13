df4ec4360e392034b6e731c2b3051a3b8ad143c9
ArtTikidji <67237375+ArtTikidji@users.noreply.github.com>
2022-08-13 13:53:27 +0400

    small refactoring

---
+++ light_control.h
@@ -0,0 +1,108 @@
+
+#include <Adafruit_NeoPixel.h>
+#include <time.h>
+
+#define SENSOR_PIN  6
+#define PIN 8   // light control pin
+#define PIN_NUM 2 //count of leds
+
+#define WAIT_FOR_SOMEONE 0
+#define INCREASE_BRIGHTNESS 1
+#define LIGHT_ON_WAIT 2
+#define TRY_OFF_LIGHT 3
+
+const int statesCount = 4;
+
+/*
+ current state of system:
+ 0 -- waiting to turn light
+ 1 -- turning on light
+ 2 -- waiting to turn off light
+ 3 -- checking sensor and keep illuminate if something detected
+ */
+int state = WAIT_FOR_SOMEONE;
+
+
+class Light_control{
+private:
+    /*
+     time shifts between iterations in each state in mills
+     in state 0 we just waiting for sensor activation, that's why states_timers[0] = NULL
+     in state 1 we have 255 brightness increesing with delay 100 mills
+     in state 2 we one iteration with waiting long time
+     in state 3 we one iteration with checking sensor and turn off if time ends
+     */
+    unsigned long states_timers[statesCount] = {NULL, 100, 240000, 30000};
+    unsigned long states_starts[statesCount] = {NULL, 0, 0, 0};
+    Adafruit_NeoPixel strip;
+    
+    unsigned long start_checking;
+    
+    unsigned long current_time;
+    
+    int brightness_increase(){
+        int brightness = (current_time - states_starts[INCREASE_BRIGHTNESS])/states_timers[INCREASE_BRIGHTNESS];
+        if (brightness > 255) {return LIGHT_ON_WAIT;}
+        strip.setPixelColor(0, strip.Color(brightness, brightness, 0));
+        strip.show();
+        return INCREASE_BRIGHTNESS;
+    }
+    
+    void turn_off_light(){
+        strip.setPixelColor(0, strip.Color(0, 0, 0));
+        strip.show();
+    }
+    
+    
+public:
+    void setup_script(){
+        strip = Adafruit_NeoPixel(PIN_NUM, PIN, NEO_GRB + NEO_KHZ800);
+        strip.begin();
+    }
+    
+    void update_current_time(){ current_time = millis(); }
+    
+    int waiting_movment(){
+        int result = digitalRead(SENSOR_PIN);
+        if (result == INCREASE_BRIGHTNESS){
+            states_starts[INCREASE_BRIGHTNESS] = current_time;
+            Serial.println("Somebody is in this area!");
+        }
+        return result;
+    }
+    
+    int smoothly_light_on(){
+        int result = brightness_increase();
+        if (result == LIGHT_ON_WAIT){
+            states_starts[LIGHT_ON_WAIT] = current_time;
+            Serial.println("Maximum rightness, start waiting!");
+        }
+        return result;
+    }
+    
+    int light_turned_on_wait(){
+        int result = LIGHT_ON_WAIT;
+        if (current_time - states_starts[LIGHT_ON_WAIT] > states_timers[LIGHT_ON_WAIT]) {
+            states_starts[TRY_OFF_LIGHT] = current_time;
+            result = TRY_OFF_LIGHT;
+            Serial.println("Time out, try to turn off light");
+        }
+        return result;
+    }
+    
+    int trying_2_off_light(){
+        int result = TRY_OFF_LIGHT;
+        int sensor_starus = digitalRead(SENSOR_PIN);
+        if (sensor_starus == 1){
+            result = LIGHT_ON_WAIT;
+            states_starts[LIGHT_ON_WAIT] = current_time;
+            Serial.println("Somebody is in this area! Don't turn off light");
+        }
+        if(current_time - states_starts[TRY_OFF_LIGHT] > states_timers[TRY_OFF_LIGHT]){
+            turn_off_light();
+            result = WAIT_FOR_SOMEONE;
+            Serial.println("Turn off light");
+        }
+        return result;
+    }
+};
--- toilet_night_lamp.ino
+++ toilet_night_lamp.ino
@@ -1,121 +1,28 @@
-#include <Adafruit_NeoPixel.h>
-#include <time.h>
+#include "light_control.h"
 
-#define SENSOR_PIN  6
-#define PIN 8   // light control pin
-#define PIN_NUM 2 //count of leds
-
-#define WAIT_FOR_SOMEONE 0
-#define INCREASE_BRIGHTNESS 1
-#define LIGHT_ON_WAIT 2
-#define TRY_OFF_LIGHT 3
-
-const int statesCount = 4;
-
-/*
-  current state of system:
-  0 -- waiting to turn light
-  1 -- turning on light
-  2 -- waiting to turn off light
-  3 -- checking sensor and keep illuminate if something detected
-*/
-int state = WAIT_FOR_SOMEONE;
-/*
-  time shifts between iterations in each state in mills
-  in state 0 we just waiting for sensor activation, that's why states_timers[0] = NULL
-  in state 1 we have 255 brightness increesing with delay 100 mills
-  in state 2 we one iteration with waiting long time 
-  in state 3 we one iteration with checking sensor and turn off if time ends
-*/
-unsigned long states_timers[statesCount] = {NULL, 100, 240000, 30000};
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIN_NUM, PIN, NEO_GRB + NEO_KHZ800);
-unsigned long start_turn_on;
-unsigned long statr_waiting;
-unsigned long start_checking;
-
-int brightness_increase(unsigned long current_time){
-  int brightness = (current_time - start_turn_on)/states_timers[INCREASE_BRIGHTNESS];
-  if (brightness > 255) {
-    return LIGHT_ON_WAIT;
-  }
-  strip.setPixelColor(0, strip.Color(brightness, brightness, 0));
-  strip.show();
-  return INCREASE_BRIGHTNESS;
-}
-
-void turn_off_light(){
-  strip.setPixelColor(0, strip.Color(0, 0, 0));
-  strip.show();
-}
-
-int waiting_movment(unsigned long current_time){
-  int result = digitalRead(SENSOR_PIN);
-  if (result == INCREASE_BRIGHTNESS){
-    start_turn_on = current_time;
-    Serial.println("Somebody is in this area!");
-  }
-  return result;
-}
-
-int smoothly_light_on(unsigned long current_time){
-  int result = brightness_increase(current_time);
-  if (result == LIGHT_ON_WAIT){ 
-    statr_waiting = current_time;
-    Serial.println("Maximum rightness, start waiting!");
-  }
-  return result;
-}
-
-int light_turned_on_wait(unsigned long current_time){
-  int result = LIGHT_ON_WAIT;
-  if (current_time - statr_waiting > states_timers[LIGHT_ON_WAIT]) {
-    start_checking = current_time;
-    result = TRY_OFF_LIGHT;
-    Serial.println("Time out, try to turn off light");
-  }
-  return result;
-}
-
-int trying_2_off_light(unsigned long current_time){
-  int result = TRY_OFF_LIGHT;
-  int sensor_starus = digitalRead(SENSOR_PIN);
-  if (sensor_starus == 1){
-    result = LIGHT_ON_WAIT;
-    statr_waiting = current_time;
-    Serial.println("Somebody is in this area! Don't turn off light");
-  }
-  if(current_time - start_checking > states_timers[TRY_OFF_LIGHT]){
-    turn_off_light();
-    result = WAIT_FOR_SOMEONE;
-    Serial.println("Turn off light");
-  }
-  return result; 
-}
+Light_control light_controler;
 
 void setup(){   
   pinMode(SENSOR_PIN, INPUT);
   Serial.begin(9600);
-  strip.begin();
-  
-  // in start position turning off light
-  turn_off_light();
+  light_controler.setup_script();
 }
 
 
 void loop(){
-  unsigned long current_time = millis();
+  light_controler.update_current_time();
   switch (state) {
     case WAIT_FOR_SOMEONE:
-      state = waiting_movment(current_time);
+      state = light_controler.waiting_movment();
       break;
     case INCREASE_BRIGHTNESS:
-      state = smoothly_light_on(current_time);
+      state = light_controler.smoothly_light_on();
       break;
     case LIGHT_ON_WAIT:
-      state = light_turned_on_wait(current_time);
+      state = light_controler.light_turned_on_wait();
       break;
     case TRY_OFF_LIGHT:
-      state = trying_2_off_light(current_time);
+      state = light_controler.trying_2_off_light();
       break;
     default:
       Serial.print("Something went wrong!\nUnknown system state: ");
